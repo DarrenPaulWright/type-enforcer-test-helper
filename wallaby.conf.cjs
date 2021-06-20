051cb25089ec29a748b862a46249a95f60ad7a80
DarrenPaulWright <darren@darrenpaulwright.com>
2021-06-19 20:59:47 -0500

    Better Wallaby config

--- wallaby.conf.cjs
+++ wallaby.conf.cjs
@@ -1,5 +1,4 @@
 const { wallabyConfig } = require('karma-webpack-bundle');
+const { name } = require('./package.json');
 
-module.exports = wallabyConfig(undefined, {
-	name: 'type-enforcer-test-helper'
-});
+module.exports = wallabyConfig(undefined, { name });
